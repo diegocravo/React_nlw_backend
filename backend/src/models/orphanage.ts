fa6b74cd9273ffed53ec13f11b8e74b77203434a
Diego Cravo Teixeira <cravo.diego@gmail.com>
2020-10-16 06:32:21 -0300

    backend terminado

--- backend/src/models/orphanage.ts
+++ backend/src/models/orphanage.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany, JoinColumn } from "typeorm";
+import Image from "./image";
 
 @Entity('orphanages')
 export default class Orphanage {
@@ -18,4 +19,10 @@ export default class Orphanage {
     opening_hours: string;
     @Column()
     open_on_weekends: boolean;
+
+    @OneToMany(() => Image, image => image.orphanage, {
+        cascade: ['insert', 'update']
+    })
+    @JoinColumn({ name: 'orphanage_id' })
+    images: Image[];
 }
